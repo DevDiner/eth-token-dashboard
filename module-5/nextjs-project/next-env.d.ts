0d30563573f497f72ed7b46875379b8578341443
devdiner <0xirwin@gmail.com>
2025-04-19 19:47:22 +0800

    added .env.local.example

--- module-5/nextjs-project/next-env.d.ts
+++ module-5/nextjs-project/next-env.d.ts
@@ -3,12 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-export {}; // mark as module so global augmentation works
-
-declare global {
-  namespace NodeJS {
-    interface ProcessEnv {
-      NEXT_PUBLIC_ALCHEMY_API_KEY: string;
-    }
-  }
-}
